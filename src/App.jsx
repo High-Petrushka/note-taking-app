92f76301011c75e34d2f4c511bd7a24c3c493ad0
High-Petrushka <daniilprokopovic12@gmail.com>
2025-05-23 10:42:24 +0300

    Making adding and deleting logic

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,73 @@ function App() {
   const [toDoList, setToDoList] = useState([]);
   const [doneList, setDoneList] = useState([]);
 
+  const [newTask, setNewTask] = useState("");
+
+  function handleInp(e) {
+    setNewTask(e.target.value);
+  }
+
+  function findTaskIndex(taskId) {
+    for (let i = 0; i < toDoList.length; i++) {
+      if (toDoList[i].id == taskId) {
+        return i;
+      }
+    }
+
+    return null
+  }
+
+  function addTask() {
+    let taskId;
+
+    if (toDoList.length == 0) {
+      taskId = 0;
+    } else {
+      taskId = toDoList[toDoList.length - 1].id + 1
+    }
+    setToDoList([
+      ...toDoList,
+      { id: taskId, text: newTask },
+    ]);
+
+    setNewTask("");
+  }
+
+  function delTask(e) {
+    const taskId = e.target.value;
+
+    setToDoList(toDoList.filter((task) => task.id != taskId));
+  }
+
   return (
     <>
       <MainHeader />
+      <div className="cont content__card">
+        <div className="list__cont">
+          <div className="title__box">
+            <h3 className="list__title">To Do</h3>
+          </div>
+          <div className="list__box">
+            <TaskList toDoList={JSON.stringify(toDoList)} onClick={delTask} />
+          </div>
+        </div>
+        <div className="list__cont">
+          <div className="title__box">
+            <h3 className="list__title">Done</h3>
+          </div>
+          <div className="list__box">
+            <ReadyList />
+          </div>
+        </div>
+        <div className="action__cont">
+          <div className="input__box">
+            <AddInp handleInp={handleInp} newTask={newTask} />
+          </div>
+          <div className="btn__box">
+            <AddBtn addTask={addTask} newTask={newTask} />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
@@ -24,39 +88,18 @@ function MainHeader() {
       }}>
         <h1 className="main__header">Note Taking App</h1>
       </div>
-      <div className="cont content__card">
-        <div className="list__cont">
-          <div className="title__box">
-            <h3 className="list__title">To Do</h3>
-          </div>
-          <div className="list__box">
-            <TaskList />
-          </div>
-        </div>
-        <div className="list__cont">
-          <div className="title__box">
-            <h3 className="list__title">Done</h3>
-          </div>
-          <div className="list__box">
-            <ReadyList />
-          </div>
-        </div>
-        <div className="action__cont">
-          <div className="input__box">
-            <AddInp />
-          </div>
-          <div className="btn__box">
-            <AddBtn />
-          </div>
-        </div>
-      </div>
+
     </>
   )
 }
 
-function TaskList() {
+function TaskList({ toDoList, onClick }) {
   return (
-    <></>
+    <ul>
+      {JSON.parse(toDoList).map((task) => (
+        <li key={task.id} value={task.id} onClick={onClick}>{task.text}</li>
+      ))}
+    </ul>
   )
 }
 
@@ -66,18 +109,18 @@ function ReadyList() {
   )
 }
 
-function AddInp() {
+function AddInp({ handleInp, newTask }) {
   return (
     <>
-      <input type="text" placeholder='New task...' />
+      <input type="text" placeholder='New task...' value={newTask} onChange={handleInp} />
     </>
   )
 }
 
-function AddBtn() {
+function AddBtn({ addTask }) {
   return (
     <>
-      <button>Add Task</button>
+      <button onClick={addTask}>Add Task</button>
     </>
   )
 }
